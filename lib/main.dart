2dced048fd0a9ea362ce25650c8f5e971d739623
witch01 <91605714+witch01@users.noreply.github.com>
2022-12-13 22:28:47 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:sharedpreferences/savemessage.dart';
+import 'package:flutter5/savemessage.dart';
 
 void main() {
   runApp(const MyApp());
